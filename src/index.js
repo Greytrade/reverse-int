845bad35b9e8a4637243bbe3553a9a114708a332
Greytrade <97153762+Greytrade@users.noreply.github.com>
2022-06-27 08:14:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-function reverse-int(num){
-  return Number(String(num).split("").reverse().join(""))
-}
+module.exports = function reverse(n) {
+    function reverse(n) {
+        return Number(String(n).split("").reverse().join(""));
+    }
+    console.log(reverseNumber(123));
+};
