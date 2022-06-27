8b2f943e5c0f3cbae30d91dc2743a5b2b7c5edac
Greytrade <97153762+Greytrade@users.noreply.github.com>
2022-06-27 08:46:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-function reverse(num){
-  return Number(String(num).split("").reverse().join(""))
-}
+module.exports = function reverse(n) {
+    function reverse(n){
+        return Number(String(n).split("").reverse().join(""))
+      }
